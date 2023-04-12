7d19f7cec6e846ff92f1ab30d061b6f13b2b65ba
muhammet topcu <mamitopcu35@gmail.com>
2023-04-12 16:34:47 +0200

    sync

---
+++ threads/src/com/tpe/threads/MultiThreading03.java
@@ -0,0 +1,49 @@
+package com.tpe.threads;
+
+public class MultiThreading03 {
+
+	public static void main(String[] args) {
+		
+		Brackets brackets = new Brackets();
+		Thread thread1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 1; i <=7; i++) {
+					brackets.generateBrackets();
+				}
+				
+			}
+			
+		});
+		thread1.start();
+		
+		Thread thread2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 1; i <=7; i++) {
+					brackets.generateBrackets();
+				}
+				
+			}
+			
+		});
+		thread2.start();
+
+	}
+
+}
+
+class Brackets{
+	public synchronized void generateBrackets() {
+		for (int i = 1; i <=10; i++) {
+			if(i<=5) {
+				System.out.print("[");
+			}else {
+				System.out.print("]");
+			}
+		}
+		System.out.println("");
+	}
+}
